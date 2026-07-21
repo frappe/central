6da5416cad55e1339f153d4ff0ee9a82cc4faa54
Om Kate <work.omkate@gmail.com>
2026-07-21 21:14:00 +0530

    feat(notification): add Notification Event Type DocType

---
+++ central/notification/doctype/notification_event_type/notification_event_type.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2026, frappe and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class NotificationEventType(Document):
+	pass
